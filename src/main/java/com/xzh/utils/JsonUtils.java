3f260b3a4e41663fbed87a57a59e702cea62362e
xzh <anenan@qq.com>
2021-05-08 11:13:43 +0800

    JSON

---
+++ src/main/java/com/xzh/utils/JsonUtils.java
@@ -0,0 +1,35 @@
+package com.xzh.utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JSON工具
+ *
+ * @author 向振华
+ * @date 2021/05/07 13:53
+ */
+public class JsonUtils {
+
+    /**
+     * 转换json为实体类
+     *
+     * @param text
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> parse(String text, Class<T> clazz) {
+        List<T> list = new ArrayList<>();
+        Object json = JSONObject.parse(text);
+        if (json instanceof JSONObject) {
+            list.add(JSONObject.parseObject(json.toString(), clazz));
+        } else if (json instanceof JSONArray) {
+            list = JSONObject.parseArray(json.toString(), clazz);
+        }
+        return list;
+    }
+}
