b4748eb27292c76238a1c73fd83dab07f7e1aa6c
xzh <anenan@qq.com>
2021-09-13 10:01:17 +0800

    日期转换工具

---
+++ src/main/java/com/xzh/utils/DateConvertUtils.java
@@ -0,0 +1,32 @@
+package com.xzh.utils;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * 日期转换工具
+ *
+ * @author 向振华
+ * @date 2021/09/13 10:00
+ */
+public class DateConvertUtils {
+
+    public static Date asDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date asDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDate asLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public static LocalDateTime asLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+}
